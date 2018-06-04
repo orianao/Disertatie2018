19acfa7f42f3e83db9ae26015bae414afb71dacd
orianao <maria.oniciuc@info.uaic.ro>
2018-06-04 22:17:45 +0300

    first good model with data centered

--- Cod/read.py
+++ Cod/read.py
@@ -7,6 +7,7 @@ from pylab import *
 import matplotlib.pyplot as plt
 import audioop
 import keras
+from sklearn.preprocessing import normalize
 from keras.models import Sequential
 from keras.layers import Conv1D, MaxPool1D, GlobalAvgPool1D, Dropout, BatchNormalization, Dense,Flatten
 from keras.optimizers import Adam
@@ -26,19 +27,46 @@ def plotit(t,samples,name):
 	fig = plt.figure()
 	fig.suptitle(name.split('_')[0])
 	the_plot = fig.add_subplot(111)
-	the_plot = plot(t[:80000], samples[:80000])
+	the_plot = plot(t, samples)
 	plt.show(block=False)
 
+def is_peak (x,left,right):
+	if np.amax(left)<x and np.amax(right)<x:
+		return True
+	return False
+
+def get_peaks(samples):
+	sol = []
+	std = np.std(samples)
+	for i in range(len(samples)):
+		if i>Cframerate and i<len(samples)-Cframerate and samples[i]>std*2.8:
+			if is_peak(samples[i],samples[i-int(Cframerate/5):i],samples[i+1:i+int(Cframerate/5)]):
+				sol.append(i)
+	return sol;
+
+def add_to_recs(samples,name,n,dirpath='a_a'):
+	i=1
+	peaks_list = get_peaks(samples)
+	for i in peaks_list:
+		recordings.append((samples[i-Cframerate:i+Cframerate],name.split('_')[0],dirpath.split('_')[1]))
+		x_train.append(samples[i-Cframerate:i+Cframerate])
+		
+		if name.split('_')[0]=="normal":
+			y_train.append(0)
+		elif name.split('_')[0] =="artifact":
+			y_train.append(1)
+		elif name.split('_')[0]=="extrahls":
+			y_train.append(2)
+		else:
+			y_train.append(3)
+		
+		i=i+1
 
 def readData():
 	no=0
 
 	for dirpath, dirnames, files in os.walk(topdir):
-		if no==20:
-			break
 		for name in files:
-			if no==20:
-				break
 			if name.lower().endswith(exten):
 				filepath = os.path.join(dirpath, name)
 				print(filepath)
@@ -47,30 +75,21 @@ def readData():
 				frames = f.readframes(-1)
 
 				framerate = f.getframerate()
-				state = None
+				#state = None
 
 				#frames, state = audioop.ratecv(frames, 1, 1, framerate, Cframerate, state)
 
 				samples = list(struct.unpack('h' * f.getnframes(), frames))
 				t = [float(i)/framerate for i in range(len(samples))]
 				
-				plotit(t,samples,name)
-				i=1
+				fac_norm = np.linalg.norm(samples)
+				samples = (samples / fac_norm) * 100
+				print (no+1, np.std(samples))
+				# samples = samples / np.amax(samples)
 
-				while Cframerate*i<=f.getnframes():
-					recordings.append((samples[Cframerate*(i-1):Cframerate*i],name.split('_')[0],dirpath.split('_')[1]))
-					x_train.append(samples[Cframerate*(i-1):Cframerate*i])
-					
-					if name.split('_')[0]=="normal":
-						y_train.append(0)
-					elif name.split('_')[0] =="artifact":
-						y_train.append(1)
-					elif name.split('_')[0]=="extrahls":
-						y_train.append(2)
-					else:
-						y_train.append(3)
-					
-					i=i+1
+				# plotit(t,samples,name)
+
+				add_to_recs(samples,name,f.getnframes())
 
 				no = no + 1
 
@@ -80,6 +99,9 @@ if __name__ == '__main__':
 	recordingsDF = pd.DataFrame(data=recordings, columns = ["samples","label","type"])
 	print(recordingsDF.head())
 	recordingsDF.to_csv("recordings.csv")
+
+	input()
+
 	x_train = np.stack(recordingsDF['samples'].values, axis=0)
 	y_train = keras.utils.to_categorical(y_train)
 
